1cfd28191c5819c09ea27288659414a648ce8825
Liwo42 <celian.colnat@gmail.com>
2026-03-17 08:11:34 +0100

    printf included

---
+++ ft_printf.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccolnat <ccolnat@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 08:28:44 by ccolnat           #+#    #+#             */
+/*   Updated: 2026/03/17 08:10:37 by ccolnat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+int	ft_puthex(unsigned int n, char base_type)
+{
+	int		count;
+	char	*base;
+
+	count = 0;
+	if (base_type == 'x')
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	if (n >= 16)
+		count += ft_puthex(n / 16, base_type);
+	count += ft_putchar(base[n % 16]);
+	return (count);
+}
+int	ft_parse(char arg_type, va_list args)
+{
+	int	count;
+
+	count = 0;
+	if (arg_type == '%')
+		count += ft_putchar('%');
+	else if (arg_type == 'c')
+		count += ft_putchar(va_arg(args, int));
+	else if (arg_type == 's')
+		count += ft_putstr(va_arg(args, char *));
+	else if (arg_type == 'p')
+		count += ft_putptr(va_arg(args, void *));
+	else if (arg_type == 'd')
+		count += ft_putnbr(va_arg(args, int));
+	else if (arg_type == 'i')
+		count += ft_putnbr(va_arg(args, int));
+	else if (arg_type == 'u')
+		count += ft_putunbr(va_arg(args, unsigned int));
+	else if (arg_type == 'x')
+		count += ft_puthex(va_arg(args, unsigned int), arg_type);
+	else if (arg_type == 'X')
+		count += ft_puthex(va_arg(args, unsigned int), arg_type);
+	return (count);
+}
+
+int	ft_printf(const char *input, ...)
+{
+	va_list	args;
+	int		i;
+	int		count;
+
+	count = 0;
+	i = 0;
+	va_start(args, input);
+	while (input[i])
+	{
+		if (input[i] == '%')
+			count += ft_parse(input[++i], args);
+		else
+			count += ft_putchar(input[i]);
+		i++;
+	}
+	return (count);
+}
--- push_swap.h
+++ push_swap.h
@@ -6,16 +6,17 @@
 /*   By: ccolnat <ccolnat@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 09:21:05 by ccolnat           #+#    #+#             */
-/*   Updated: 2026/03/17 07:54:24 by ccolnat          ###   ########.fr       */
+/*   Updated: 2026/03/17 08:10:30 by ccolnat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <stdarg.h>
+# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "printf/ft_printf.h"
 
 void	go_to_next(const char *str, int *j, char c);
 static char	*ft_alloc_word(char const *str, char c);
@@ -29,6 +30,15 @@ ssize_t debugg(ssize_t nb);
 size_t	ft_strlen(char const *str);
 char **split_correct_input(const char *str, char c);
 char *clean_list(const char *str);
+
+int	ft_printf(const char *input, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_parse(char arg_type, va_list args);
+int	ft_putptr(void *ptr);
+int	ft_putnbr(int n);
+int	ft_putunbr(unsigned int n);
+int	ft_puthex(unsigned int n, char base_type);
 //split_correct_input()
 //check_dup()
 #endif
